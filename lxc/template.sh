14efaee8ea30c8bb11b4b37f47c8c7a8f81f5929
John Galt <johngaltfirstrun@gmail.com>
2025-09-16 13:45:00 -0400

    Revert "lxc/template: add sddm sleep hack"
    This reverts commit 9a44f133111a96406114391f6bc4885c036a67a4.
    
    No longer needed

--- lxc/template.sh
+++ lxc/template.sh
@@ -452,13 +452,6 @@ display_creation_message() {
   fi
 }
 
-sddm_sleep_hack() {
-  mkdir -p "${LXC_ROOTFS}/etc/systemd/system/sddm.service.d"
-  cat <<EOF > "${LXC_ROOTFS}/etc/systemd/system/sddm.service.d/override.conf"
-[Service]
-ExecStartPre=/bin/sleep 8
-EOF
-}
 
 create_build_dir
 unpack_metadata
@@ -469,7 +462,6 @@ set_utsname
 add_base_config
 replace_template_vars
 fix_tty
-sddm_sleep_hack
 
 display_creation_message
 
